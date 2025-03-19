e64aa4da72f2d72f5f048f30f9a77a3ac307e4bb
Ananya9876 <ananyamnaik6@gmail.com>
2025-03-19 09:03:18 +0530

    CSK RCB

---
+++ Hello/src/com/xworkz/anu/prog/DisplayRunner.java
@@ -0,0 +1,11 @@
+package com.xworkz.anu.prog;
+
+public class DisplayRunner {
+        public static void main(String[] args) {
+            Shop shop = new Shop();
+            Display display = new Display();
+
+            shop.ads(display); // Valid case
+            shop.ads(null); // Null case
+        }
+    }
--- Hello/src/com/xworkz/anu/prog/Rocket.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.anu.prog;
-
-public class Rocket {
-    public static void main(String[] args) {
-        System.out.println("GBcjgvghnf");
-    }
-}
