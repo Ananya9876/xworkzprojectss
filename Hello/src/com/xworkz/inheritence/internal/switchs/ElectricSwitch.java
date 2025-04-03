02183cf3a83c5a43c883d09447a106c38456a197
Ananya9876 <ananyamnaik6@gmail.com>
2025-04-03 23:51:24 +0530

    Inheritance

---
+++ Hello/src/com/xworkz/inheritence/internal/switchs/ElectricSwitch.java
@@ -0,0 +1,10 @@
+package com.xworkz.inheritence.internal.switchs;
+
+import com.xworkz.inheritence.switchs.Switch;
+
+public class ElectricSwitch extends Switch {
+    public ElectricSwitch() {
+        super();
+        System.out.println("Running non-arg constructor ElectricSwitch");
+    }
+}
---
+++ Hello/src/com/xworkz/inheritence/internal/temple/HinduTemple.java
@@ -0,0 +1,10 @@
+package com.xworkz.inheritence.internal.temple;
+
+import com.xworkz.inheritence.temple.Temple;
+
+public class HinduTemple extends Temple {
+    public HinduTemple() {
+        super();
+        System.out.println("Running non-arg constructor HinduTemple");
+    }
+}
