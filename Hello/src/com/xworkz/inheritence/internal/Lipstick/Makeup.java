fb11e4a5308a2056425a72357defeec768dd11be
Ananya9876 <ananyamnaik6@gmail.com>
2025-04-04 08:42:45 +0530

    Inheritance

---
+++ Hello/src/com/xworkz/inheritence/internal/Lipstick/Makeup.java
@@ -0,0 +1,10 @@
+package com.xworkz.inheritence.internal.Lipstick;
+
+import com.xworkz.inheritence.internal.laptop.Laptop;
+
+public class Makeup extends Lipstick {
+    public Makeup() {
+        super();
+        System.out.println("Running non-arg constructor Makeup");
+    }
+}
