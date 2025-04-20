d36f8a099746b4f6ef5766506ef6e81cd3ccee37
Ananya9876 <ananyamnaik6@gmail.com>
2025-04-20 23:22:20 +0530

    interface,implements

--- Hello/src/com/xworkz/interfaces/implements1/Runnerinterface1.java
+++ Hello/src/com/xworkz/interfaces/implements1/Runnerinterface1.java
@@ -10,455 +10,532 @@ public class Runnerinterface1 {
         obj.attach();
         obj.detach();
         obj.clean();
+        obj.wear();
         System.out.println("************************************************************");
         Actuator actuator = new ActuatorImpl();
         actuator.engage();
         actuator.disengage();
         actuator.calibrate();
+        actuator.checkstatus();
         System.out.println("************************************************************");
         Appliance appliance = new ApplianceImpl();
         appliance.turnOn();
         appliance.turnOff();
+        appliance.repair();
         appliance.repair();
         System.out.println("************************************************************");
         AppliancePart appliancePart = new AppliancePartImpl();
         appliancePart.insert();
         appliancePart.eject();
         appliancePart.inspect();
+        appliancePart.remove();
         System.out.println("************************************************************");
         Battery battery = new BatteryImpl();
         battery.charge();
         battery.discharge();
         battery.recycle();
+        battery.batterydown();
         System.out.println("************************************************************");
         Belt belt = new BeltImpl();
         belt.tension();
         belt.move();
         belt.slip();
+        belt.color();
         System.out.println("************************************************************");
 
         Blade blade = new BladeImpl();
         blade.cut();
         blade.sharpen();
         blade.store();
+        blade.shape();
         System.out.println("************************************************************");
 
         Bolt bolt = new BoltImpl();
         bolt.secure();
         bolt.unscrew();
         bolt.fasten();
+        bolt.slower();
         System.out.println("************************************************************");
 
         Button button = new ButtonImpl();
         button.press();
         button.release();
         button.reset();
+        button.shell();
         System.out.println("************************************************************");
 
         Cable cable = new CableImpl();
         cable.plugIn();
         cable.transmit();
         cable.unplug();
+        cable.plug();
         System.out.println("************************************************************");
 
         Caliper caliper = new CaliperImpl();
         caliper.measure();
         caliper.read();
         caliper.reset();
+        caliper.write();
         System.out.println("************************************************************");
 
         Casing casing = new CasingImpl();
         casing.enclose();
         casing.shield();
         casing.label();
+        casing.open();
         System.out.println("************************************************************");
 
         Charger charger = new ChargerImpl();
         charger.connect();
         charger.power();
         charger.disconnect();
+        charger.poweron();
         System.out.println("************************************************************");
 
         Chassis chassis = new ChassisImpl();
         chassis.mount();
         chassis.hold();
         chassis.align();
+        chassis.leave();
         System.out.println("************************************************************");
 
         Chisel chisel = new ChiselImpl();
         chisel.carve();
         chisel.chip();
         chisel.shape();
+        chisel.size();
         System.out.println("************************************************************");
 
         Clamp clamp = new ClampImpl();
         clamp.fix();
         clamp.adjust();
         clamp.lock();
+        clamp.error();
         System.out.println("************************************************************");
 
         Compass compass = new CompassImpl();
         compass.draw();
         compass.adjust();
         compass.measure();
+        compass.fix();
         System.out.println("************************************************************");
 
         Component component = new ComponentImpl();
         component.fit();
         component.remove();
         component.test();
+        component.adjust();
         System.out.println("************************************************************");
 
         Connector connector = new ConnectorImpl();
         connector.link();
         connector.secure();
         connector.release();
+        connector.info();
         System.out.println("************************************************************");
 
         Controller controller = new ControllerImpl();
         controller.program();
         controller.execute();
         controller.reset();
+        controller.debug();
         System.out.println("************************************************************");
 
         Cover cover = new CoverImpl();
         cover.place();
         cover.remove();
         cover.protect();
+        cover.clean();
         System.out.println("************************************************************");
 
         Device device = new DeviceImpl();
         device.powerUp();
         device.powerDown();
         device.reset();
+        device.run();
         System.out.println("************************************************************");
 
         Display display = new DisplayImpl();
         display.show();
         display.refresh();
         display.clear();
+        display.remove();
         System.out.println("************************************************************");
 
         Door door = new DoorImpl();
         door.swing();
         door.latch();
         door.unlock();
+        door.lock();
         System.out.println("************************************************************");
 
         Drill drill = new DrillImpl();
         drill.bore();
         drill.reverse();
         drill.stop();
+        drill.work();
         System.out.println("************************************************************");
 
         Engine engine = new EngineImpl();
         engine.ignite();
         engine.throttle();
         engine.idle();
+        engine.engineon();
         System.out.println("************************************************************");
 
         Equipment equipment = new EquipmentImpl();
         equipment.install();
         equipment.uninstall();
         equipment.update();
+        equipment.edit();
         System.out.println("************************************************************");
 
         Fan fan = new FanImpl();
         fan.blow();
         fan.oscillate();
         fan.cool();
+        fan.swing();
         System.out.println("************************************************************");
 
         Filter filter = new FilterImpl();
         filter.trap();
         filter.clean();
         filter.replace();
+        filter.filter();
         System.out.println("************************************************************");
 
         Floor floor = new FloorImpl();
         floor.lay();
         floor.tile();
         floor.polish();
+        floor.total();
         System.out.println("************************************************************");
 
         Foundation foundation = new FoundationImpl();
         foundation.set();
         foundation.level();
         foundation.cure();
+        foundation.build();
         System.out.println("************************************************************");
 
         Frame frame = new FrameImpl();
         frame.support();
         frame.secure();
         frame.stabilize();
+        frame.hang();
         System.out.println("************************************************************");
         Furniture furniture = new FurnitureImpl();
         furniture.assemble();
         furniture.disassemble();
         furniture.polish();
+        furniture.price();
         System.out.println("************************************************************");
 
         Gadget gadget = new GadgetImpl();
         gadget.activate();
         gadget.deactivate();
         gadget.troubleshoot();
+        gadget.reboot();
         System.out.println("************************************************************");
 
         Gear gear = new GearImpl();
         gear.rotate();
         gear.interlock();
         gear.transfer();
+        gear.paddle();
         System.out.println("************************************************************");
 
         Hammer hammer = new HammerImpl();
         hammer.hit();
         hammer.pull();
         hammer.tap();
+        hammer.beat();
         System.out.println("************************************************************");
 
         Handle handle = new HandleImpl();
         handle.grip();
         handle.twist();
         handle.pull();
+        handle.use();
         System.out.println("************************************************************");
 
         Hinge hinge = new HingeImpl();
         hinge.pivot();
         hinge.align();
         hinge.rust();
+        hinge.bust();
         System.out.println("************************************************************");
 
         Hose hose = new HoseImpl();
         hose.connect();
         hose.flow();
         hose.disconnect();
+        hose.ring();
         System.out.println("************************************************************");
 
         Housing housing = new HousingImpl();
         housing.surround();
         housing.protect();
         housing.vent();
+        housing.floor();
         System.out.println("************************************************************");
 
         Instrument instrument = new InstrumentImpl();
         instrument.play();
         instrument.tune();
         instrument.clean();
+        instrument.measure();
         System.out.println("************************************************************");
 
         Knob knob = new KnobImpl();
         knob.rotate();
         knob.adjust();
         knob.calibrate();
+        knob.knot();
         System.out.println("************************************************************");
 
         Level level = new LevelImpl();
         level.measure();
         level.align();
         level.indicate();
+        level.place();
         System.out.println("************************************************************");
 
         Lever lever = new LeverImpl();
         lever.pull();
         lever.push();
         lever.lock();
+        lever.unlock();
         System.out.println("************************************************************");
 
         Lock lockObj = new LockImpl();  // 'lock' is a keyword in some contexts
         lockObj.secure();
         lockObj.open();
         lockObj.jam();
+        lockObj.key();
         System.out.println("************************************************************");
 
         Machine machine = new MachineImpl();
         machine.operate();
         machine.shutDown();
         machine.calibrate();
+        machine.run();
         System.out.println("************************************************************");
 
         Module module = new ModuleImpl();
         module.integrate();
         module.isolate();
         module.test();
+        module.examin();
         System.out.println("************************************************************");
 
         Motor motor = new MotorImpl();
         motor.run();
         motor.speedUp();
         motor.slowDown();
+        motor.ring();
         System.out.println("************************************************************");
 
         Nail nail = new NailImpl();
         nail.drive();
         nail.hold();
         nail.pull();
+        nail.color();
         System.out.println("************************************************************");
 
         Nut nut = new NutImpl();
         nut.twist();
         nut.grip();
         nut.hold();
+        nut.squize();
         System.out.println("************************************************************");
 
         Panel panel = new PanelImpl();
         panel.open();
         panel.close();
         panel.lock();
+        panel.space();
         System.out.println("************************************************************");
 
         Pipe pipe = new PipeImpl();
         pipe.carry();
         pipe.leakCheck();
         pipe.insulate();
+        pipe.repair();
         System.out.println("************************************************************");
 
         Plane plane = new PlaneImpl();
         plane.shave();
         plane.level();
         plane.finish();
+        plane.clean();
         System.out.println("************************************************************");
 
         Processor processor = new ProcessorImpl();
         processor.compute();
         processor.analyze();
         processor.optimize();
+        processor.defuse();
         System.out.println("************************************************************");
 
         Pump pump = new PumpImpl();
         pump.suction();
         pump.pressure();
         pump.stop();
+        pump.repair();
         System.out.println("************************************************************");
 
         Roof roof = new RoofImpl();
         roof.cover();
         roof.drain();
         roof.ventilate();
+        roof.shield();
         System.out.println("************************************************************");
 
         Sander sander = new SanderImpl();
         sander.smooth();
         sander.finish();
         sander.clean();
+        sander.shine();
         System.out.println("************************************************************");
 
         Saw saw = new SawImpl();
         saw.cut();
         saw.guide();
         saw.rest();
+        saw.see();
         System.out.println("************************************************************");
         Screw screw = new ScrewImpl();
         screw.thread();
         screw.tighten();
         screw.loosen();
+        screw.remove();
         System.out.println("************************************************************");
 
         Seal seal = new SealImpl();
         seal.compress();
         seal.stretch();
         seal.hold();
+        seal.measure();
         System.out.println("************************************************************");
 
         Sensor sensor = new SensorImpl();
         sensor.detect();
         sensor.measure();
         sensor.alert();
+        sensor.color();
         System.out.println("************************************************************");
 
         Shaft shaft = new ShaftImpl();
         shaft.spin();
         shaft.hold();
         shaft.balance();
+        shaft.move();
         System.out.println("************************************************************");
 
         Spring spring = new SpringImpl();
         spring.compress();
         spring.release();
         spring.bounce();
+        spring.ring();
         System.out.println("************************************************************");
 
         Square square = new SquareImpl();
         square.check();
         square.mark();
         square.align();
+        square.measure();
         System.out.println("************************************************************");
 
         Structure structure = new StructureImpl();
         structure.build();
         structure.strengthen();
         structure.maintain();
+        structure.trackSteps();
         System.out.println("************************************************************");
 
         Switch switchObj = new SwitchImpl(); // 'switch' is a Java keyword
         switchObj.toggle();
         switchObj.enable();
         switchObj.disable();
+        switchObj.insert();
         System.out.println("************************************************************");
 
         Tank tank = new TankImpl();
         tank.fill();
         tank.drain();
         tank.seal();
+        tank.quantity();
         System.out.println("************************************************************");
 
         TapeMeasure tapeMeasure = new TapeMeasureImpl();
         tapeMeasure.extend();
         tapeMeasure.retract();
         tapeMeasure.read();
+        tapeMeasure.preserve();
         System.out.println("************************************************************");
 
         Tool tool = new ToolImpl();
         tool.use();
         tool.maintain();
         tool.store();
+        tool.operate();
         System.out.println("************************************************************");
 
         Utility utility = new UtilityImpl();
         utility.run();
         utility.monitor();
         utility.terminate();
+        utility.trackSteps();
         System.out.println("************************************************************");
 
         Valve valve = new ValveImpl();
         valve.open();
         valve.close();
         valve.regulate();
+        valve.clean();
         System.out.println("************************************************************");
 
         Vehicle vehicle = new VehicleImpl();
         vehicle.start();
         vehicle.stop();
         vehicle.refuel();
+        vehicle.drive();
         System.out.println("************************************************************");
 
         Wall wall = new WallImpl();
         wall.erect();
         wall.paint();
         wall.insulate();
+        wall.display();
         System.out.println("************************************************************");
 
         Washer washer = new WasherImpl();
         washer.cushion();
         washer.space();
         washer.seal();
+        washer.wash();
         System.out.println("************************************************************");
 
         Wheel wheel = new WheelImpl();
         wheel.spin();
         wheel.stop();
         wheel.align();
+        wheel.throwing();
         System.out.println("************************************************************");
 
         Window window = new WindowImpl();
         window.open();
         window.close();
         window.lock();
+        window.color();
         System.out.println("************************************************************");
 
         Wrench wrench = new WrenchImpl();
         wrench.twist();
         wrench.turn();
         wrench.hold();
+        wrench.use();
         System.out.println("************************************************************");
 
     }
