d759607cb2ed7425c4713dc8c9cba92e08377a67
Ananya9876 <ananyamnaik6@gmail.com>
2025-04-21 23:32:04 +0530

    Reverse

--- Hello/src/com/xworkz/practiceprogram/Reverse.java
+++ Hello/src/com/xworkz/practiceprogram/Reverse.java
@@ -4,11 +4,19 @@ public class Reverse {
         public static void main(String[] args){
 
             int[] arr={1,2,3,4,5};
+            char[] letter={'A','N','A','N','Y','A'};
             int reverse;
+            int rev;
 
             for(int i=arr.length-1;i>=0;i--){
                 reverse=arr[i];
                 System.out.print(reverse+" ");
+            }
+            System.out.println();
+
+            for(int j=letter.length-1;j>=0;j--){
+                rev=letter[j];
+                System.out.print(rev+" ");
             }
         }
 }
