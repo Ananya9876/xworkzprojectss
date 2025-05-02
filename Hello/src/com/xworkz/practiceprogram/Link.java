926465817112350e9f6a961ab18eede0f316df16
Ananya9876 <ananyamnaik6@gmail.com>
2025-05-02 18:41:34 +0530

    Pattern

---
+++ Hello/src/com/xworkz/practiceprogram/Link.java
@@ -0,0 +1,20 @@
+package com.xworkz.practiceprogram;
+
+
+import java.util.Stack;
+
+public class Link {
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+
+        if(!st.isEmpty()){
+            System.out.println(st.pop());
+            st.peek();
+        }
+
+    }
+}
